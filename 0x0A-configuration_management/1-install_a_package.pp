6ebdf54e5e1dfbe436a244fbf84fff4a9fd62bbd
Jikrou <jamaleddine.zait@gmail.com>
2024-06-28 13:33:20 +0000

    Install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,12 @@
-# install flask package
-package { 'flask':
+# insatll flask using pip3
+
+package { 'Werkzeug':
+  ensure   => '2.0.1',
+  provider => 'pip3',
+}
+
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  }
+  require  => Package['Werkzeug'],
+}
