6ca24e8da59b7e95e8cf4739dae0aaf9d92f543c
Jikrou <jamaleddine.zait@gmail.com>
2024-07-30 12:21:01 +0000

    data

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,22 +7,23 @@ import requests
 import sys
 
 
-employee_id = sys.argv[1]
-base_url = "https://jsonplaceholder.typicode.com"
+if __name__ == "__main__":
+    employee_id = sys.argv[1]
+    base_url = "https://jsonplaceholder.typicode.com"
 
-employee_rsp = requests.get(f"{base_url}/users/{employee_id}").json()
-EMPLOYEE_NAME = employee_rsp['name']
+    employee_rsp = requests.get(f"{base_url}/users/{employee_id}").json()
+    EMPLOYEE_NAME = employee_rsp['name']
 
-TOTAL_NUMBER_OF_TASKS = requests.get(
-        f"{base_url}/users/{employee_id}/todos").json()
+    TOTAL_NUMBER_OF_TASKS = requests.get(
+            f"{base_url}/users/{employee_id}/todos").json()
 
-NUMBER_OF_DONE_TASKS = sum(1 for t in TOTAL_NUMBER_OF_TASKS if t['completed'])
+    NUMBER_OF_DONE_TASKS = sum(
+            1 for t in TOTAL_NUMBER_OF_TASKS if t['completed'])
 
-print(
-        f"{EMPLOYEE_NAME} is done with tasks("
-        f"{NUMBER_OF_DONE_TASKS}/{len(TOTAL_NUMBER_OF_TASKS)}): ")
+    print(
+            f"{EMPLOYEE_NAME} is done with tasks("
+            f"{NUMBER_OF_DONE_TASKS}/{len(TOTAL_NUMBER_OF_TASKS)}): ")
 
-
-for t in TOTAL_NUMBER_OF_TASKS:
-    if t['completed']:
-        print(f"\t{t['title']}")
+    for t in TOTAL_NUMBER_OF_TASKS:
+        if t['completed']:
+            print(f"\t{t['title']}")
