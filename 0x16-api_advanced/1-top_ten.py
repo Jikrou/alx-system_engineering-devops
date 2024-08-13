e9c3b3932cd6687e2fad12f80f0307a3e6e76bba
jikrou <jamaleddine.zait@gmail.com>
2024-08-13 19:49:14 +0000

    topT

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,7 +3,7 @@
 Function print the titles for the first 10 hot posts for a given subreddit
 """
 
-import requests
+from requests import get
 
 
 def top_ten(subreddit):
@@ -18,13 +18,13 @@ def top_ten(subreddit):
 
     user_agent = {'User-agent': 'MyPythonScript/1.0'}
     url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-    response = requests.get(url, headers=user_agent, allow_redirects=False)
+    response = get(url, headers=user_agent, allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json()
-        posts = data.get('data', {}).get('children', [])
+        posts = data.get('data').get('children')
         for post in posts[:10]:
-            print(post.get('data', {}).get('title', 'Not title found'))
+            print(post.get('data').get('title'))
 
     else:
         print(None)
